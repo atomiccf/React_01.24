e7aa50588a088a72553480e632b628c3ff2ac0ed
VasilyeuViacheslau <106859411+atomiccf@users.noreply.github.com>
2024-02-28 17:47:19 +0100

    -- added animation to the main header -- added animation to the buttons

--- src/components/MenuButton/MenuButton.tsx
+++ src/components/MenuButton/MenuButton.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import css from './MenuButton.module.css'
+import {motion} from 'framer-motion'
 import {MenuButtonProps} from '../../types/types.ts'
 
 export const MenuButton: React.FC<MenuButtonProps> = ({text, handleButton}) => {
   return (
-    <button onClick={handleButton} className={css.menu_button}>
+    <motion.button whileTap={{scale: 0.75}} onClick={handleButton} className={css.menu_button}>
       {text}
-    </button>
+    </motion.button>
   )
 }
